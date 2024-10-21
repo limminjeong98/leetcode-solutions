ee53ab8e2200d6b3b903c2c8552e4a356ecc26f1
minjeong <48610633+limminjeong98@users.noreply.github.com>
2024-10-21 11:32:14 +0900

    Update coin-change.java

--- coin-change.java
+++ coin-change.java
@@ -5,17 +5,16 @@ import java.util.*;
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        // 금액을 동전으로 바꾸는 것이 불가능하다면 amount + 1 개
+        // 0원은 가능하므로 0으로 두기
+        Arrays.fill(dp, 1, amount + 1, amount + 1);
 
-        for (int money = 1; money <= amount; money++) {
-            for (int coin : coins) {
-                if (money >= coin && dp[money - coin] != Integer.MAX_VALUE) {
-                    dp[money] = Math.min(dp[money], dp[money - coin] + 1);
-                }
+        for (int coin : coins) {
+            for (int money = coin; money <= amount; money++) {
+                dp[money] = Math.min(dp[money], dp[money - coin] + 1);
             }
         }
 
-        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }

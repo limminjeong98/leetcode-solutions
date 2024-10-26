445d61d8023b51b3b71cf623026e3fd308aad3de
minjeong <48610633+limminjeong98@users.noreply.github.com>
2024-10-26 19:07:55 +0900

    Create maximum-subarray.java

---
+++ maximum-subarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+// https://leetcode.com/problems/maximum-subarray
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            max = Math.max(sum, max);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}

f1d5a7a6d84e8c3baf00a9042cab9cdae8c422b2
minjeong <48610633+limminjeong98@users.noreply.github.com>
2024-10-26 18:50:32 +0900

    Create fibonacci-number.java

---
+++ fibonacci-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+// https://leetcode.com/problems/fibonacci-number
+
+class Solution {
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+}

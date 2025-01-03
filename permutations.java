22e56f91d1355d5d2bb998b9527694d86e9e0e14
minjeong <48610633+limminjeong98@users.noreply.github.com>
2025-01-03 16:58:36 +0900

    Update permutations.java

--- permutations.java
+++ permutations.java
@@ -3,34 +3,27 @@ import java.util.*;
 // https://leetcode.com/problems/permutations
 
 class Solution {
-    public static List<List<Integer>> answer;
-  
     public List<List<Integer>> permute(int[] nums) {
-        answer = new ArrayList<>();
-      
-        List<Integer> notVisited = Arrays.stream(nums).boxed().collect(Collectors.toList());
-      
-        dfs(new ArrayList<Integer>(), notVisited); 
-      
+        List<List<Integer>> answer = new LinkedList<>();
+        boolean[] visited = new boolean[nums.length];
+        dfs(answer, new LinkedList<>(), nums, visited, nums.length, 0);
         return answer;
     }
 
-    public void dfs(List<Integer> visited, List<Integer> notVisited) {
-        if (notVisited.isEmpty()) {
-            answer.add(new ArrayList<>(visited));
+    // 다음에 방문할 숫자를 List<Integer>로 관리하는 것보다, boolean[] 으로 방문처리하는거 더 빠름
+    public void dfs(List<List<Integer>> answer, List<Integer> current, int[] nums, boolean[] visited, int n, int count) {
+        if (count == n) {
+            answer.add(new LinkedList<>(current));
             return;
         }
 
-        // int i 로 하면 int -> Integer 박싱 연산 발생함
-        for (Integer i : notVisited) {
-            List<Integer> newVisited = new ArrayList<>(notVisited);
-            // remove 메서드 인자로 int가 오면 인덱스 삭제하고, Integer가 오면 값을 삭제함
-            // newVisited.remove(Integer.valueOf(i));
-            newVisited.remove(i);
-
-            visited.add(i);
-            dfs(visited, newVisited);
-            visited.removeLast();
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            current.add(nums[i]);
+            dfs(answer, current, nums, visited, n, count + 1);
+            current.removeLast();
+            visited[i] = false;
         }
     }
 }
